04937e123470a2f8d763a0f34f57386a7d8362e1
vyx <121124647+v-y-x@users.noreply.github.com>
2026-07-05 13:28:43 +0200

    new feature and a few changes
    landmine feature! every message has a chance to timeout the user for 1m, 3m or cause a nuke, which times out them and nearby users.
    
    the global message count now works by counting the amount of lines that are in the current messages.txt
    
    when replying to the bot, it has a five second cooldown before being able to reply again.
    
    &markov command has a 240 second cooldown now.

--- bot.py
+++ bot.py
@@ -5,6 +5,7 @@ from discord.ext import tasks
 import markovify
 import logging
 import random
+import time
 from datetime import timedelta
 from dotenv import load_dotenv
 load_dotenv()
@@ -23,11 +24,18 @@ intents = discord.Intents.default()
 intents.message_content = True
 
 bot = commands.Bot(command_prefix='&', intents=intents)
-globalMsg = 57300
+
+def get_line_count(): # get the current global message count, equal to the amount of lines in messages.txt
+    if not os.path.exists('messages.txt'):
+        return 0
+    with open('messages.txt', encoding='utf-8') as f:
+        return sum(1 for _ in f)
+
+globalMsg = get_line_count()
 
 @bot.command()
 async def scrape(ctx, amount : int = 1000):
-    if ctx.author.id != 548578270808113173:
+    if ctx.author.id != 548578270808113173: # prevent anyone except me from executing
         await ctx.send('not authorized')
         return
 
@@ -52,9 +60,9 @@ async def scrape(ctx, amount : int = 1000):
     await progMsg.edit(content=f"{msgCount} messages scraped! current total: {globalMsg}")
     
 @bot.command()
-async def token(ctx):
+async def token(ctx): # i don't even know why i made this
     await ctx.send(f"OPSEC LEVEL: SIGMA DEMON. NO TOKEN FOR U BLUD")
-    await ctx.author.timeout(timedelta(minutes=1))
+    await ctx.author.timeout(timedelta(minutes=1)) 
 
 @bot.command()
 async def total(ctx):
@@ -62,38 +70,75 @@ async def total(ctx):
     await ctx.send(f"my current message count is {globalMsg}")
 
 @bot.command()
-@commands.cooldown(1, 180, commands.BucketType.default)
+@commands.cooldown(1, 240, commands.BucketType.default)
 async def markov(ctx):
+    global text_model
     sentence = text_model.make_sentence(tries=100)
     print(repr(sentence))
     if sentence:
         await ctx.send(sentence)
+
+last_reply_time = 0
+REPLY_CD = 5
 
 @bot.event
 async def on_message(message):
     if message.author.bot:
         return
     
+    global text_model, last_reply_time
+
     if message.reference and message.reference.resolved: # is the message a reply to something?
         replied_to = message.reference.resolved 
-
         if bot.user and replied_to.author.id == bot.user.id: # is the reply directed towards the bot?
-            sentence = text_model.make_short_sentence(80, tries=100)
-            if sentence:
-                await message.reply(sentence)
+            now = time.time()
+            if now - last_reply_time >= REPLY_CD: # checks whether the last replying message is more than REPLY_CD seconds ago
+                sentence = text_model.make_short_sentence(80, tries=100)
+                if sentence:
+                    await message.reply(sentence)
+                    last_reply_time = now
 
     global globalMsg
     if message.content:
         store_message(message.content)
         globalMsg += 1
         
-    if random.random() < 0.02:
+    if random.random() < 0.02: # 2% chance
         sentence = text_model.make_short_sentence(140, tries=100)
         print(repr(sentence))
         if sentence:
             await message.channel.send(sentence)
 
-    await bot.process_commands(message)
+    if random.random() < 0.005: # .5% chance
+        try:
+            if random.random() < .2: # 20% chance
+                await message.author.timeout(timedelta(minutes=3))
+                await message.channel.send(f'{message.author.mention} stepped on a super landmine! 💥 timed out for 3m')
+
+            elif random.random() < .1: # 10% chance
+                await message.author.timeout(timedelta(minutes=1))
+                await message.channel.send(f'{message.author.mention} stepped on a nuke!! ☢️💥 5 people are timed out for 1m')
+
+                messages = []
+                async for msg in message.channel.history(limit=5):
+                    messages.append(msg)
+
+                for msg in reversed(messages):
+                    if msg.author.id == message.author.id: # skip the user that triggered this, cause they're already muted from earlier
+                        continue
+                    try:
+                        await msg.author.timeout(timedelta(minutes=1))
+                        await message.channel.send(f'{msg.author.mention} is poisoned by fallout! ☢️')
+                    except discord.Forbidden:
+                        await message.channel.send(f'{msg.author.mention} is immune to fallout! ☢️') # fall back if no perms to timeout
+
+            else:
+                await message.author.timeout(timedelta(minutes=1))
+                await message.channel.send(f'{message.author.mention} stepped on a landmine! 💥 timed out for 1m')
+        except discord.Forbidden:
+            await message.channel.send(f'{message.author.mention} stepped on a land mine 💥, but is immune!') # fall back if no perms to timeout
+
+    await bot.process_commands(message) 
 
 @bot.event
 async def on_command_error(ctx, error):
@@ -112,12 +157,11 @@ async def on_ready():
 async def hello(ctx):
       await ctx.channel.send('Hello!')
 
-@tasks.loop(minutes=5)
+@tasks.loop(minutes=5) # task loop that refreshes markov model every 5 minutes
 async def rebuild():
     global text_model
     with open("messages.txt", encoding="utf-8") as f:
         text = f.read()
         text_model = markovify.Text(text)
-
 
 bot.run(bot_token, log_handler=handler)
